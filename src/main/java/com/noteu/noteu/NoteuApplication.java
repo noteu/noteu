7b513ac8d29cf76b5422f9a57f7e3e7b1c223838
kurt <qodydgus97@kakao.com>
2023-11-03 12:38:42 +0900

    초기세팅

---
+++ src/main/java/com/noteu/noteu/NoteuApplication.java
@@ -0,0 +1,13 @@
+package com.noteu.noteu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noteu/noteu/NoteuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noteu.noteu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
