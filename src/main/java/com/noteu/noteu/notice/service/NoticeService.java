8eef0a70896038b5aff99aa098e8be7814d57177
chaewoniiii <102212394+chaewoniiii@users.noreply.github.com>
2023-11-21 09:59:35 +0900

    #37 feat: Notice
    - 공지사항 CRUD 로직
    - 공지사항 html, js

--- src/main/java/com/noteu/noteu/notice/service/NoticeService.java
+++ src/main/java/com/noteu/noteu/notice/service/NoticeService.java
@@ -1,4 +1,14 @@
 package com.noteu.noteu.notice.service;
 
+import com.noteu.noteu.notice.dto.NoticeRequestDto;
+import com.noteu.noteu.notice.dto.NoticeResponseDto;
+
+import java.util.ArrayList;
+
 public interface NoticeService {
+    NoticeResponseDto save(NoticeRequestDto noticeRequestDto, Long memberId, Long subjectId);
+    NoticeResponseDto getNotice(Long id);
+    ArrayList<NoticeResponseDto> getAll(Long subjectId);
+    void updateById(NoticeRequestDto noticeRequestDto, Long id);
+    void delNotice(Long id);
 }
