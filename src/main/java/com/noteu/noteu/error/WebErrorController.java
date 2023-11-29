acf30d859dc3c6b46b7f527c86ad61d5d88beca9
kurt <qodydgus97@kakao.com>
2023-11-29 20:14:31 +0900

    #108 feat : 에러페이지 추가

---
+++ src/main/java/com/noteu/noteu/error/WebErrorController.java
@@ -0,0 +1,30 @@
+package com.noteu.noteu.error;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status != null){
+            int statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "layout/error/pages-404";
+            } else {
+                return "layout/error/pages-500";
+            }
+        }
+
+        return "layout/error/pages-maintenance";
+    }
+
+}
