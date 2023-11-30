3ed5afd57a676318b4e84411424c9241914fd093
chaewoniiii <102212394+chaewoniiii@users.noreply.github.com>
2023-11-30 16:57:23 +0900

    #132 fix: subject list.js
    - 과목 코드 눌렀을 때 복사가 안되는 버그 해결

--- src/main/resources/static/js/subject/list.js
+++ src/main/resources/static/js/subject/list.js
@@ -1,10 +1,15 @@
 function copy_to_clipboard(subjectCodeId) {
     const copyCode = document.getElementById(subjectCodeId).value;
-    navigator.clipboard.writeText(copyCode)
-        .then(() => {
-            alert('과목 코드 복사 완료!');
-        })
-        .catch((error) => {
-            console.error('클립보드 복사 실패:', error);
-        });
+
+    try {
+        const textArea = document.createElement('textarea');
+        textArea.value = copyCode;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        alert('과목 코드 복사 완료!');
+    } catch (error) {
+        console.error('클립보드 복사 실패:', error);
+    }
 }
